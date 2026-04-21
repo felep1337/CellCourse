7eff2f3d778750cf24d64b015cd039e7d0dea4eb
Филипп Фуга <cyhka312@mail.ru>
2026-04-21 22:54:35 +0500

    Filter segmentation plus Kalman(???)

--- test.py
+++ test.py
@@ -2,32 +2,13 @@ import numpy as np
 import czifile
 import napari
 from skimage import filters
-from skimage.segmentation import clear_border
 import cv2
 from scipy import ndimage as ndi
-from skimage import measure
 
 
 def segment_cells(image):
     image = np.power(image, 0.7)
 
-    # img_normalized = (image - image.min()) / (image.max() - image.min() + 1e-10)
-    #
-    # img_blurred = cv2.GaussianBlur(img_normalized.astype(np.float32), (5, 5), 1)
-    #
-    # threshold_value = filters.threshold_otsu(img_blurred)
-    #
-    # binary = img_blurred > threshold_value
-    #
-    # binary = ndi.binary_fill_holes(binary)
-    # labeled = measure.label(binary)
-    # labeled = clear_border(labeled)
-    # binary = labeled > 0
-    #
-    # binary_uint8 = (binary * 255).astype(np.uint8)
-    # contours, _ = cv2.findContours(binary_uint8, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #
-    # return contours
     img = (image - image.min()) / (image.max() - image.min() + 1e-10)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
@@ -41,11 +22,7 @@ def segment_cells(image):
 
     binary = ndi.binary_fill_holes(binary)
 
-    binary = clear_border(binary)
-
-    contours, _ = cv2.findContours((binary * 255).astype(np.uint8),
-                                   cv2.RETR_EXTERNAL,
-                                   cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours((binary * 255).astype(np.uint8),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     filtered = []
     for c in contours:
@@ -57,30 +34,60 @@ def segment_cells(image):
 
     return filtered
 
+
+class KalmanTrack:
+    def __init__(self, x, y):
+        self.kf = cv2.KalmanFilter(4, 2)
+
+        self.kf.transitionMatrix = np.array([[1,0,1,0],
+                                             [0,1,0,1],
+                                             [0,0,1,0],
+                                             [0,0,0,1]], np.float32)
+
+        self.kf.measurementMatrix = np.array([[1,0,0,0],
+                                              [0,1,0,0]], np.float32)
+
+        self.kf.statePre = np.array([[x],[y],[0],[0]], np.float32)
+
+        self.points = [(int(x), int(y))]
+
 def process_multiple_frames_czi(filepath):
     print(f"Файл {filepath}")
+
     with czifile.CziFile(filepath) as czi:
         image_data = czi.asarray()
         print(f"Размерность: {image_data.shape}")
 
-        n_frames = 1#image_data.shape[0]
-        viewer = napari.Viewer(title="Клеточки:3")
+        n_frames = image_data.shape[0]
+        viewer = napari.Viewer(title="Трекинг клеток")
 
         masks = []
         images = []
 
-        for frame in range(n_frames):
+        for frame in range(1):
             img = np.mean(image_data[frame, :, :, :], axis=2).astype(np.float32)
+
             contours = segment_cells(img)
+
+            centers = []
+            for c in contours:
+                x, y, w, h = cv2.boundingRect(c)
+                cx = x + w // 2
+                cy = y + h // 2
+                centers.append([cx, cy])
+
             mask = np.zeros_like(img, dtype=np.uint8)
             for contour in contours:
-                cv2.polylines(mask, [contour.astype(np.int32)], True, 255, thickness=2)
+                cv2.polylines(mask, [contour.astype(np.int32)], True, 255, 2)
+
             masks.append(mask)
             images.append(img)
 
+        traj = np.zeros_like(images[0], dtype=np.float32)
+
+
         viewer.add_image(np.stack(images), name="Оригинал")
-        viewer.add_image(np.stack(masks), name="Контур", colormap='blue',
-                         blending='additive', opacity=0.7)
+        viewer.add_image(np.stack(masks), name="Контуры",colormap='red', blending='additive')
 
         napari.run()
 
